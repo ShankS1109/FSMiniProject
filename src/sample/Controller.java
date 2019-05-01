1a7afbc35f37a2d82baf4e883a655e01427573f4
Shashank Singh <shashu.singh11sept@gmail.com>
2019-05-01 18:05:30 +0530

    i havw finished with the gui only main file indexing is left

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,12 @@
 package sample;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.Event;
 import javafx.fxml.FXML;
-        import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
         import javafx.scene.control.ChoiceBox;
         import javafx.scene.control.DatePicker;
         import javafx.scene.control.RadioButton;
@@ -10,14 +16,65 @@ import javafx.fxml.FXML;
         import javafx.scene.layout.AnchorPane;
         import javafx.scene.layout.StackPane;
 
-public class  Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class  Controller implements Initializable{
+
+    private int count = 0,id;
+    private int index=0;
+    private String loc,name1,phno,eadd,packagee;
+    private int DaysBooking=4;//4 days 3 nights
+    private float[] cost_factor={1.0f ,0.5f};
+    private float total=0.0f;
+    private int[] LocCost={100000,175000,250000};
+    ObservableList<String> locationList = FXCollections.observableArrayList("Amsterdam","Bali","Cyprus","Egypt","Japan","London",
+            "Paris","Singapore","Thailand");
         @FXML
         private Button next;
+        @FXML
+        void nextpage(ActionEvent event){
+            System.out.println("next button clicked!");
+            namesync();
+            if(count==0){
+                page1.setVisible(false);
+                page2.setVisible(true);
+                page3.setVisible(false);
+                count++;
+                prev.setDisable(false);
+            }
+            else if(count==1){
+                page1.setVisible(false);
+                page2.setVisible(false);
+                page3.setVisible(true);
+                count++;
+                prev.setDisable(false);
+            }
+
+        }
+
+    @FXML
+        private Button prev;
 
         @FXML
-        private Button cancel;
+         void prevpage(ActionEvent event){
+            System.out.println("previous button clicked!");
+            if(count==1){
+                page1.setVisible(true);
+                page2.setVisible(false);
+                page3.setVisible(false);
+                count--;
+                prev.setDisable(true);
+            }
+            else if(count==2){
+                page1.setVisible(false);
+                page2.setVisible(true);
+                page3.setVisible(false);
+                count--;
+                prev.setDisable(false);
+            }
 
+        }
         @FXML
         private StackPane stackid;
 
@@ -29,6 +86,13 @@ public class  Controller {
 
         @FXML
         private TextField name;
+        @FXML
+        void namesync(){
+                System.out.println(name.getText());
+                booking.setText(name.getText());
+                booking.setEditable(false);
+            }
+
 
         @FXML
         private TextField persons;
@@ -40,7 +104,13 @@ public class  Controller {
         private DatePicker date;
 
         @FXML
-        private ChoiceBox<?> location;
+        private ChoiceBox Location;
+
+        @FXML
+        void getLocation(){
+            loc = Location.getValue().toString();
+            System.out.println(loc);
+        }
 
         @FXML
         private RadioButton economy;
@@ -58,13 +128,85 @@ public class  Controller {
         private RadioButton terms;
 
         @FXML
+        void nextcontrol(ActionEvent event){
+            if(terms.isSelected()){
+                next.setDisable(false);
+                finish.setDisable(false);
+            }
+            else{
+                next.setDisable(true);
+                finish.setDisable(true);
+                count=1;
+                prevpage(event);
+            }
+
+        }
+
+        @FXML
         private TextArea packagedeal;
+
+        @FXML
+        void TextChange(){
+            if(economy.isSelected()){
+                index=0;
+                packagee = "Economy";
+                packagedeal.setText("        COST :Rs. 100000\n" +
+                                    "            per person\n"+
+                                    "\nIncludes:-\n" +
+                                    "Flight tickets,\n" +
+                                    "Airport transfers,\n" +
+                                    "Accommodation at 3 star hotel,\n" +
+                                    "4 Days & 5 Nights\n"+
+                                    "double sharing,\n" +
+                                    "Attractions,\n" +
+                                    "Buffet breakfast & Dinner,\n" +
+                                    "Tour bus to and from the sights");
+            }
+            else if(gold.isSelected()){
+                packagee="Gold";
+                index=1;
+                packagedeal.setText("        COST :Rs. 175000\n" +
+                                    "            per person\n"+
+                                    "\nIncludes:-\n" +
+                                    "Flight tickets,\n" +
+                                    "Airport transfers,\n" +
+                                    "Accommodation at 3 star hotel,\n" +
+                                    "4 Days & 5 Nights\n"+
+                                    "Deluxe rooms in hotel\n"+
+                                    "Lunch inclusive on all the days\n" +
+                                    "Attractions,\n" +
+                                    "Buffet breakfast & Dinner,\n" +
+                                    "Tour bus to and from the sights");
+            }
+            else if(platinum.isSelected()){
+                packagee = "platinum";
+                index=2;
+                packagedeal.setText("        COST :Rs. 250000\n" +
+                                    "            per person\n"+
+                                    "\nIncludes:-\n" +
+                                    "Business class flight tickets\n" +
+                                    "Airport transfers,\n" +
+                                    "Accommodation at 4 star hotel,\n" +
+                                    "4 Days & 5 Nights\n" +
+                                    "Suites room at hotel\n"+
+                                    "Lunch inclusive on all the days\n" +
+                                    "Attractions,\n" +
+                                    "Buffet breakfast & Dinner,\n" +
+                                    "chauffeur driven car provided");
+            }
+            }
+
+
 
         @FXML
         private AnchorPane page2;
 
         @FXML
         private TextField booking;
+        @FXML
+        void getbooking(){
+            booking.setText(name.getText());
+        }
 
         @FXML
         private TextField phonenum;
@@ -90,4 +232,25 @@ public class  Controller {
         @FXML
         private Button finish;
 
+        @FXML
+        void save(){
+            String code = T_id.getText();
+            id = Integer.parseInt(code);
+            name1 = name.getText();
+            eadd = email.getText();
+            phno = phonenum.getText();
+            //loc has location, packagee has package
+            DaysBooking = Integer.parseInt(days.getText());
+            //total =
+
+
+        }
+
+        @Override@FXML
+    public void initialize(URL location, ResourceBundle resources) {
+        Location.setItems(locationList);
+        economy.setSelected(true);
     }
+    public Controller(){}
+
+}
